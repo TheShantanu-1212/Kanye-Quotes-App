c8c8736e33855df6a504a0e9be630a6d1740f0bd
TheShantanu-1212 <skoti126@gmail.com>
2025-06-26 11:33:44 +0530

    feat: raise exception if response code is not OK and change background color to red

--- main.py
+++ main.py
@@ -2,11 +2,15 @@ import requests
 from tkinter import *
 
 ENDPOINT = "https://api.kanye.rest/"
+RED = "#EA2F14"
+ORANGE = "#FB9E3A"
 
 
 def get_quote():
     """Fetch and display a random Kanye West quote from the API."""
-    quote = requests.get(url=ENDPOINT).json()["quote"]
+    response = requests.get(url=ENDPOINT)
+    response.raise_for_status()
+    quote = response.json()["quote"]
     canvas.itemconfig(quote_text, text=quote)
 
 
@@ -15,10 +19,10 @@ def get_quote():
 # Creating the main window
 window = Tk()
 window.title("Kanye Says...")
-window.config(padx=50, pady=50, bg="white")
+window.config(padx=50, pady=50, bg=RED)
 
 # Creating the canvas to display background image and quote
-canvas = Canvas(width=300, height=414, bg="white", highlightthickness=0)
+canvas = Canvas(width=300, height=414, bg=RED, highlightthickness=0)
 background_img = PhotoImage(file="assets/background.png")
 canvas.create_image(150, 207, image=background_img)
 quote_text = canvas.create_text(
@@ -27,14 +31,18 @@ quote_text = canvas.create_text(
     text="Kanye Quote Goes HERE",
     width=250,
     font=("Arial", 20, "bold"),
-    fill="white",
+    fill="black",
 )
 canvas.grid(row=0, column=0)
 
 # Creating the Kanye button
 kanye_img = PhotoImage(file="assets/kanye.png")
 kanye_button = Button(
-    image=kanye_img, highlightthickness=0, command=get_quote, bg="white"
+    image=kanye_img,
+    highlightthickness=0,
+    command=get_quote,
+    bg=RED,
+    activebackground="yellow",
 )
 kanye_button.grid(row=1, column=0)
 
